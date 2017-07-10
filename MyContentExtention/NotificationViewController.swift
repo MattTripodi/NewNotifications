ed14bc8cfe9929b07eaf2deab2daa30501bffaf1
Matt Tripodi <mtripodi1027@gmail.com>
2017-07-09 21:15:00 -0400

    Created Rick Grimes notification

---
+++ MyContentExtention/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  MyContentExtention
+//
+//  Created by Matt Tripodi on 7/9/17.
+//  Copyright © 2017 Matt Tripodi. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
